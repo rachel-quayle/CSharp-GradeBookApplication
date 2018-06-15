03a6adef15bca31e8c188504f2e444f0bec03c18
Rachel Quayle <rachel.quayle@utmostwealth.com>
2018-06-15 11:46:52 +0100

    Increase Parts Check to 3

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -33,15 +33,25 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type og gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2].ToLower();
+            BaseGradeBook gradebook;
+            if (type == "standard")
+                gradebook = new StandardGradeBook(name);
+            else if (type == "ranked")
+                gradebook = new RankedGradeBook(name);
+            else
+            {
+                Console.WriteLine("{0} is not supported type of gradebook, please try again.", type);
+                    return;
+            }
             Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            GradeBookUserInterface.CommandLoop(gradebook);
         }
 
         public static void LoadCommand(string command)
